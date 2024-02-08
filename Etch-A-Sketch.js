3e1093e9aeab328395dcc9c3790b49512236c68e
Knight1E <AYellowPixel@gmail.com>
2024-02-08 01:58:59 -0600

    Made a Manual Grid Base

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -16,13 +16,13 @@ var gridDivP = document.createElement("p");
 // gridDivP.textContent = "Flexbox created successfully.";
 gridDiv.appendChild(gridDivP);
 gridDiv.style.backgroundColor = "pink";
-gridDiv.style.width = "500px";
-gridDiv.style.height = "500px";
+gridDiv.style.width = "300px";
+gridDiv.style.height = "300px";
 gridDiv.style.margin = "auto";
 
 var gridRow = document.createElement("div");
 gridRow.className = "flexKidRow";
-gridRow.textContent = "Row created successfully.";
+// gridRow.textContent = "Row created successfully.";
 gridRow.style.cssText = "width: 90%; height: 90%; background-color: #ff82b4; margin: auto;"
 
 /*var gridRowItem = document.createElement("div");
@@ -31,11 +31,26 @@ gridRowItem.style.cssText = "margin: auto; flex: 1; width: 100px; height: 100px;
 gridRow.appendChild(gridRowItem);
 gridRow.appendChild(gridRowItem);*/
 
-for (let i = 0; i < 5; i++) {
-    var gridRowItem = document.createElement("div");
-    gridRowItem.style.cssText = "margin: auto; flex: 1; width: 10px; height: 10px; background-color: #98eb78; border-style: solid; flex-direction: row;";
-    gridRow.appendChild(gridRowItem);
+// Testing the grid w/ manual input.
+var gridSizeNumber = 10;
+
+for (let i = 0; i < gridSizeNumber; i++) {
+    let gridSize = (100 / gridSizeNumber) + "%"; // We'd replace the 5 width the # of squares we'd have.
+    var gridRowLoop = document.createElement("div");
+    gridRowLoop.style.cssText = "width: 100%; height: 100%; background-color: blue; margin: auto;";
+
+    // A row of 5
+    for (let i = 0; i < gridSizeNumber; i++) {
+        var gridRowItem = document.createElement("div");
+        gridRowItem.style.cssText = "margin: auto; flex: 1; width: 100%; background-color: #98eb78; border-style: solid;";
+        gridRowItem.style.height = gridSize;
+        gridRowLoop.appendChild(gridRowItem);
+    }
+
+    gridDiv.appendChild(gridRowLoop);
 }
 
-gridDiv.appendChild(gridRow);
+
+
+// gridDiv.appendChild(gridRow);
 gridContainer.appendChild(gridDiv);
